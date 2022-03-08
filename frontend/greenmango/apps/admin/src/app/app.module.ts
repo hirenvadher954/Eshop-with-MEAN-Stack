400078c8afa6ea4f3000336f05b1765597fe2abc
hiren <hirenvadher954@gmail.com>
2022-03-08 23:52:04 +0530

    Product list updated

--- frontend/greenmango/apps/admin/src/app/app.module.ts
+++ frontend/greenmango/apps/admin/src/app/app.module.ts
@@ -1,29 +1,31 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { RouterModule, Routes } from "@angular/router";
-import { HttpClientModule } from "@angular/common/http";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppComponent } from "./app.component";
-import { DashboardComponent } from "./pages/dashboard/dashboard.component";
-import { SidebarComponent } from "./shared/sidebar/sidebar.component";
-import { ShellComponent } from "./shared/shell/shell.component";
-import { CategoriesListComponent } from "./pages/categories/categories-list/categories-list.component";
-import { CategoriesFormComponent } from "./pages/categories/categories-form/categories-form.component";
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { ShellComponent } from './shared/shell/shell.component';
+import { CategoriesListComponent } from './pages/categories/categories-list/categories-list.component';
+import { CategoriesFormComponent } from './pages/categories/categories-form/categories-form.component';
 
-import { CategoriesService } from "@greenmango/products";
-import { CardModule } from "primeng/card";
-import { ToolbarModule } from "primeng/toolbar";
-import { ButtonModule } from "primeng/button";
-import { TableModule } from "primeng/table";
-import { InputTextModule } from "primeng/inputtext";
-import { ToastModule } from "primeng/toast";
-import { ConfirmDialogModule } from "primeng/confirmdialog";
-import { ColorPickerModule } from "primeng/colorpicker";
-import { ConfirmationService, MessageService } from "primeng/api";
+import { CategoriesService } from '@greenmango/products';
+import { CardModule } from 'primeng/card';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ColorPickerModule } from 'primeng/colorpicker';
+import { ConfirmationService, MessageService } from 'primeng/api';
 import { ProductsFormComponent } from './pages/products/products-form/products-form.component';
 import { ProductsListComponent } from './pages/products/products-list/products-list.component';
+import { UsersFormComponent } from './pages/users/users-form/users-form.component';
+import { UsersListComponent } from './pages/users/users-list/users-list.component';
 
 const UX_MODULE = [
   CardModule,
@@ -33,21 +35,22 @@ const UX_MODULE = [
   ToolbarModule,
   ButtonModule,
   ConfirmDialogModule,
-  ColorPickerModule
+  ColorPickerModule,
 ];
 const routes: Routes = [
   {
-    path: "",
+    path: '',
     component: ShellComponent,
     children: [
-      { path: "dashboard", component: DashboardComponent },
-      { path: "categories", component: CategoriesListComponent },
-      { path: "categories/form", component: CategoriesFormComponent },
-      {
-        path: "categories/form/:id", component: CategoriesFormComponent
-      }
-    ]
-  }
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'categories', component: CategoriesListComponent },
+      { path: 'categories/form', component: CategoriesFormComponent },
+      { path: 'categories/form/:id', component: CategoriesFormComponent },
+      { path: 'products', component: ProductsListComponent },
+      { path: 'products/form', component: ProductsFormComponent },
+      { path: 'products/form/:id', component: ProductsFormComponent },
+    ],
+  },
 ];
 
 @NgModule({
@@ -59,7 +62,9 @@ const routes: Routes = [
     CategoriesListComponent,
     CategoriesFormComponent,
     ProductsFormComponent,
-    ProductsListComponent
+    ProductsListComponent,
+    UsersFormComponent,
+    UsersListComponent,
   ],
   imports: [
     BrowserModule,
@@ -67,11 +72,10 @@ const routes: Routes = [
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(routes, { initialNavigation: "enabledBlocking" }),
-    ...UX_MODULE
+    RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
+    ...UX_MODULE,
   ],
   providers: [CategoriesService, MessageService, ConfirmationService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
--- frontend/greenmango/apps/admin/src/app/pages/products/products-form/products-form.component.ts
+++ frontend/greenmango/apps/admin/src/app/pages/products/products-form/products-form.component.ts
@@ -1,16 +1,89 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import {
+  CategoriesService,
+  Category,
+  Product,
+  ProductsService,
+} from '@greenmango/products';
+import { MessageService } from 'primeng/api';
+import { timer } from 'rxjs';
 
 @Component({
-  selector: 'greenmango-products-form',
+  selector: 'admin-products-form',
   templateUrl: './products-form.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class ProductsFormComponent implements OnInit {
+  editmode = false;
+  form: FormGroup;
+  isSubmitted = false;
+  catagories: Category[] = [];
+  imageDisplay: string | ArrayBuffer;
+  currentProductId: string;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private productsService: ProductsService,
+    private categoriesService: CategoriesService,
+    private messageService: MessageService,
+    private location: Location,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this._initForm();
+    this._getCategories();
+    this._checkEditMode();
   }
 
+  private _initForm() {
+    this.form = this.formBuilder.group({
+      name: ['', Validators.required],
+      brand: ['', Validators.required],
+      price: ['', Validators.required],
+      category: ['', Validators.required],
+      countInStock: ['', Validators.required],
+      description: ['', Validators.required],
+      richDescription: [''],
+      image: ['', Validators.required],
+      isFeatured: [false],
+    });
+  }
+
+  private _getCategories() {
+    this.categoriesService.getCategories().subscribe((categories) => {
+      this.catagories = categories;
+    });
+  }
+  private _checkEditMode() {
+    this.route.params.subscribe((params) => {
+      if (params['id']) {
+        this.editmode = true;
+        this.currentProductId = params['id'];
+        this.productsService
+          .getProduct(params['id'])
+          .subscribe((product: Product) => {
+            this.productForm['name'].setValue(product.name);
+            this.productForm['category'].setValue(product.category.id);
+            this.productForm['brand'].setValue(product.brand);
+            this.productForm['price'].setValue(product.price);
+            this.productForm['countInStock'].setValue(product.countInStock);
+            this.productForm['isFeatured'].setValue(product.isFeatured);
+            this.productForm['description'].setValue(product.description);
+            this.productForm['richDescription'].setValue(
+              product.richDescription
+            );
+            this.imageDisplay = product.image;
+            this.productForm['image'].setValidators([]);
+            this.productForm['image'].updateValueAndValidity();
+          });
+      }
+    });
+  }
+  get productForm() {
+    return this.form.controls;
+  }
 }
